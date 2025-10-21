857d1a9a8d031f3a1fbbe99193667ceb08ab7ea3
Abdo73873 <abdelrahman73873.com>
2025-10-21 15:18:11 +0400

    js update

---
+++ lib/src/browser/ecdh.dart
@@ -0,0 +1,160 @@
+// Copyright 2019-2020 Gohilla.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:math';
+import 'dart:typed_data';
+
+import 'package:cryptography/cryptography.dart';
+
+import '_javascript_bindings.dart' as web_crypto;
+import 'browser_ec_key_pair.dart';
+
+class BrowserEcdh extends Ecdh {
+  final int length;
+
+  @override
+  final KeyPairType keyPairType;
+
+  /// ECDH with P-256.
+  factory BrowserEcdh.p256({
+    required int length,
+    Random? random,
+  }) {
+    return BrowserEcdh._(
+      KeyPairType.p256,
+      length: length,
+    );
+  }
+
+  /// ECDH with P-384.
+  factory BrowserEcdh.p384({
+    required int length,
+    Random? random,
+  }) {
+    return BrowserEcdh._(
+      KeyPairType.p384,
+      length: length,
+    );
+  }
+
+  /// ECDH with P-521.
+  factory BrowserEcdh.p521({
+    required int length,
+    Random? random,
+  }) {
+    return BrowserEcdh._(
+      KeyPairType.p521,
+      length: length,
+    );
+  }
+
+  const BrowserEcdh._(
+    this.keyPairType, {
+    required this.length,
+  }) : super.constructor();
+
+  @override
+  Future<EcKeyPair> newKeyPair({
+    bool isExtractable = true,
+    bool allowSign = true,
+  }) async {
+    return BrowserEcKeyPair.generate(
+      keyPairType: keyPairType,
+      isExtractable: isExtractable,
+      allowSign: allowSign,
+      allowDeriveBits: true,
+    );
+  }
+
+  @override
+  Future<EcKeyPair> newKeyPairFromSeed(List<int> seed) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<SecretKey> sharedSecretKey({
+    required KeyPair keyPair,
+    required PublicKey remotePublicKey,
+  }) async {
+    if (keyPair is! EcKeyPair) {
+      throw ArgumentError.value(
+        keyPair,
+        'localKeyPair',
+        'Should be an instance of EcKeyPair, not: $keyPair',
+      );
+    }
+    final jsPrivateKeyFuture = BrowserEcKeyPair.from(
+      keyPair,
+      isExtractable: false,
+      allowSign: false,
+      allowDeriveBits: true,
+    );
+    final jsPublicKeyFuture = jsPublicKeyFrom(
+      remotePublicKey,
+      webCryptoCurve: keyPairType.webCryptoCurve!,
+    );
+    final jsPrivateKey = await jsPrivateKeyFuture;
+    final jsPublicKey = await jsPublicKeyFuture;
+    try {
+      final byteBuffer = await web_crypto.deriveBits(
+        web_crypto.EcdhKeyDeriveParams(
+          name: 'ECDH',
+          public: jsPublicKey,
+        ),
+        jsPrivateKey.jsPrivateKeyForEcdh!,
+        8 * length,
+      );
+      return SecretKey(Uint8List.view(byteBuffer));
+    } catch (error, stackTrace) {
+      throw StateError(
+        'Web Cryptography throw an error: $error\n$stackTrace',
+      );
+    }
+  }
+
+  static Future<web_crypto.CryptoKey> jsPublicKeyFrom(
+    PublicKey publicKey, {
+    required String webCryptoCurve,
+  }) async {
+    if (publicKey is! EcPublicKey) {
+      throw ArgumentError.value(
+        publicKey,
+        'publicKey',
+        'Should be EcPublicKey',
+      );
+    }
+    try {
+      return await web_crypto.importKeyWhenJwk(
+        web_crypto.Jwk(
+          kty: 'EC',
+          crv: webCryptoCurve,
+          ext: true,
+          key_ops: const ['deriveBits'],
+          x: web_crypto.base64UrlEncode(publicKey.x),
+          y: web_crypto.base64UrlEncode(publicKey.y),
+        ),
+        web_crypto.EcKeyImportParams(
+          name: 'ECDH',
+          namedCurve: webCryptoCurve,
+        ),
+        true,
+        const [],
+      );
+    } catch (error, stackTrace) {
+      throw StateError(
+        'Web Cryptography throw an error: $error\n$stackTrace',
+      );
+    }
+  }
+}
---
+++ lib/src/browser/hkdf.dart
@@ -0,0 +1,67 @@
+// Copyright 2019-2020 Gohilla.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:typed_data';
+
+import 'package:cryptography/cryptography.dart';
+import 'package:cryptography/src/browser/hash.dart';
+
+import '_javascript_bindings.dart' show jsArrayBufferFrom;
+import '_javascript_bindings.dart' as web_crypto;
+
+/// HKDF implementation that uses _Web Cryptography API_ in browsers.
+///
+/// See [BrowserCryptography].
+class BrowserHkdf extends Hkdf {
+  @override
+  final Hmac hmac;
+
+  @override
+  final int outputLength;
+
+  const BrowserHkdf({required this.hmac, required this.outputLength})
+      : super.constructor();
+
+  @override
+  Future<SecretKeyData> deriveKey({
+    required SecretKey secretKey,
+    List<int> nonce = const <int>[],
+    List<int> info = const <int>[],
+  }) async {
+    final jsCryptoKey = await _jsCryptoKey(secretKey);
+    final byteBuffer = await web_crypto.deriveBits(
+      web_crypto.HkdfParams(
+        name: 'HKDF',
+        hash: BrowserHashAlgorithmMixin.hashAlgorithmNameFor(
+          hmac.hashAlgorithm,
+        )!,
+        salt: jsArrayBufferFrom(nonce),
+        info: jsArrayBufferFrom(info),
+      ),
+      jsCryptoKey,
+      8 * outputLength,
+    );
+    return SecretKeyData(Uint8List.view(byteBuffer));
+  }
+
+  Future<web_crypto.CryptoKey> _jsCryptoKey(SecretKey secretKey) async {
+    final secretKeyBytes = await secretKey.extractBytes();
+    return await web_crypto.importKeyWhenRaw(
+      web_crypto.jsArrayBufferFrom(secretKeyBytes),
+      'HKDF',
+      false,
+      const ['deriveBits'],
+    );
+  }
+}
